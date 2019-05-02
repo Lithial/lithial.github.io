205a887bd424517d41b63e10f8d4f038425c0e2c
Lithial <jameshessell@gmail.com>
2019-05-02 15:48:19 +1200

    conversion to auto gen code

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,30 +1,44 @@
-// Set the date we're counting down to
-var programmingassessment2date = new Date("May 3, 2019 17:00:00").getTime();
-var ProgrammingAssessment3date = new Date("Friday, 7 June 2019, 17:00").getTime();
-var IntroToMobile2date = new Date("Tuesday, 11 June 2019 08:15:00").getTime();
-var IntrotoData1 = new Date("Friday, 24 May 2019 8:15:00").getTime();
-var IntrotoData2 = new Date("Monday, 10 June 2019 17:00:00").getTime();
-var InfoSystems1 = new Date("Thursday, May 30 2019 08:15:00").getTime();
-//var IntrotoData2 = new Date().getTime();
-//var InfoSystems2 = new Date().getTime();
-//var InfoSystems3 = new Date().getTime();
-//var IntroToMobile3date = new Date().getTime();
+var requestURL = 'https://lithial.github.io/scripts/data.json';
+//change to scripts after when git push
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  var classes = request.response;
+  console.log(classes);
+  classes.forEach(element => {
+    if(element.date != "")
+    {
+      createBlock(element.name, element.date, element.class);
+    }
+  });
+}
 
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+function createBlock(name, date, paper){
+  var newDiv = document.createElement("div");
+  var brElement = document.createElement("br");
+  var brElement2 = document.createElement("br");
+  var countdown = document.createElement("p")
+  countdown.setAttribute("id",name);
 
-// Update the count down every 1 second
-var a = setInterval(function(){timer(programmingassessment2date,"Programming Grader");}, 1000);
-var b = setInterval(function(){timer(ProgrammingAssessment3date,"Programming Movie Database");}, 1000);
-var c = setInterval(function(){timer(IntroToMobile2date, "Intro To Mobile Prototype");},1000);
-var e = setInterval(function(){timer(IntrotoData1, "Data Sql Report");},1000);
-var g = setInterval(function(){timer(IntrotoData2, "Data Ethics Report");},1000);
-var h = setInterval(function(){timer(InfoSystems1, "Info Systems Report");},1000);
-//var f = setInterval(function(){timer(IntrotoData2, "Data Ethics Report");},1000)
-//var i = setInterval(function(){timer(InfoSystems2, "Info Systems Practical");},1000)
-//var d = setInterval(function(){timer(IntroToMobile3date, "Intro To Mobile Test");},1000)
-//var j = setInterval(function(){timer(InfoSystems3, "Info Systems Test");},1000)
+  var newContent = document.createTextNode(name);
+  var classContent = document.createTextNode(paper);
+  var dateContent = document.createTextNode(date);
+  var inputdate = new Date(date).getTime();
 
-function timer(counter, div) {
+  newDiv.appendChild(newContent);
+  newDiv.appendChild(brElement);
+  newDiv.appendChild(classContent);
+  newDiv.appendChild(brElement2);
+  newDiv.appendChild(countdown);
+  newDiv.appendChild(dateContent);
+  
+  document.body.appendChild(newDiv);
+  var a = setInterval(function(){timer(a,inputdate, name);},1000);
+
+}
+function timer(a,counter, div) {
 
     // Get todays date and time
     var now = new Date().getTime();
@@ -42,7 +56,7 @@ function timer(counter, div) {
     + minutes + "m " + seconds + "s ";
   
     if (distance < 0) {
-      clearInterval(x);
+      clearInterval(a);
       document.getElementById(div).innerHTML = "EXPIRED";
     }
   }
